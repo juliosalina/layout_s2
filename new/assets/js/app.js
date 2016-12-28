010ef8f759b59d33b64d1cc27391ee0ef4fdc138
Julio Henrique Salina <juliohsalina@gmail.com>
2016-12-28 17:02:52 -0200

    ajuste menu fechado

--- new/assets/js/app.js
+++ new/assets/js/app.js
@@ -1,10 +1,12 @@
-//slimscroll menu lateral
-$('.itens-menu').slimScroll({
-    height: 'auto',
-    position: 'right',
-    size: "7px",
-    color: '#bbb',
-    wheelStep: 5
+$(document).ready(function() {
+    //slimscroll menu lateral
+    $('.itens-menu').slimScroll({
+        height: 'auto',
+        position: 'right',
+        size: "7px",
+        color: '#bbb',
+        wheelStep: 5
+    });
 });
 
 //menu busca icone lupa
@@ -22,31 +24,45 @@ $('.menu-button').on('click', function() {
     if ($('body').hasClass('menu-open')) {
         $('body').removeClass('menu-open');
         $('body').addClass('menu-close');
-        $('.menu').removeClass('toggle-open');
-        $('.menu').addClass('toggle-close');
+        $('.menu-fundo').removeClass('toggle-open');
+        $('.menu-fundo').addClass('toggle-close');
         $('.menu-button').removeClass('toggle-open');
         $('.menu-button').addClass('toggle-close');
         $('.title-page').removeClass('toggle-open');
         $('.title-page').addClass('toggle-close');
 
+        $('.logo a img').attr('src', 'assets/images/logom.jpg');
+
+        $('.itens-menu').slimScroll({destroy: true});
+
     } else {
         $('body').removeClass('menu-close');
         $('body').addClass('menu-open');
-        $('.menu').removeClass('toggle-close');
-        $('.menu').addClass('toggle-open');
+        $('.menu-fundo').removeClass('toggle-close');
+        $('.menu-fundo').addClass('toggle-open');
         $('.menu-button').removeClass('toggle-close');
         $('.menu-button').addClass('toggle-open');
         $('.title-page').removeClass('toggle-close');
         $('.title-page').addClass('toggle-open');
+
+        $('.logo a img').attr('src', 'assets/images/logo.jpg');
+
+        //slimscroll menu lateral
+        $('.itens-menu').slimScroll({
+            height: 'auto',
+            position: 'right',
+            size: "7px",
+            color: '#bbb',
+            wheelStep: 5
+        });
     }
 });
 
 //click menu submenu
-if (!$('body').hasClass('menu-close')) {
+var menuItemClick = function(e) {
 
-    var menuItemClick = function(e) {
+    if(!$('body').hasClass('menu-close')) {
         var $this = e;
-        console.log(e);
         if($(this).parent().hasClass("has_sub")) {
             e.preventDefault();
         }
@@ -66,8 +82,9 @@ if (!$('body').hasClass('menu-close')) {
             $(this).next("ul").slideUp(350);
             $(".pull-right i", $(this).parent()).removeClass("fa-minus").addClass("fa-plus");
         }
-    };
+    }
 
-    $menuItem = $("#sidebar-menu a");
-    $menuItem.on('click', menuItemClick);
-}
+};
+
+$menuItem = $("#sidebar-menu a");
+$menuItem.on('click', menuItemClick);
