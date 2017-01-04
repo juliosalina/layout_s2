6780e4f36ed2e18b8209b108beac9b162da08992
Julio Henrique Salina <juliohsalina@gmail.com>
2017-01-04 11:50:32 -0200

    ajuste col

--- new/templates/dashboard.php
+++ new/templates/dashboard.php
@@ -1,26 +1,26 @@
 <div class="dash row ml-0 mr-0">
-    <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3 pl-0-md pr-05-md text-xs-center flex-items-xs-stretch">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 pl-0-md pr-05-md text-xs-center flex-items-xs-stretch">
         <div class="card card-block">
             <h7 class="card-title">Total de Agendamentos</h5>
             <div class="value down mt-05 mb-05">5654</div>
             <p class="mb-0"><b>32%</b> a menos que Novembro</p>
         </div>
     </div>
-    <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3 plr-05-md pr-0-lg text-xs-center flex-items-xs-stretch">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 plr-05-md pr-0-lg text-xs-center flex-items-xs-stretch">
         <div class="card card-block">
             <h7 class="card-title">Total de Atendimentos</h5>
             <div class="value up mt-05 mb-05">4325</div>
             <p class="mb-0"><b>55%</b> a mais que Novembro</p>
         </div>
     </div>
-    <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3 plr-05-md pl-0-lg text-xs-center flex-items-xs-stretch">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 plr-05-md pl-0-lg text-xs-center flex-items-xs-stretch">
         <div class="card card-block">
             <h7 class="card-title">Nota Centro Médico</h5>
             <div class="value up mt-05 mb-05">9.4</div>
             <p class="mb-0">Nota dada pelos pacientes ao Centro Médico Sacomã</p>
         </div>
     </div>
-    <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3 pr-0-md pl-05-md text-xs-center flex-items-xs-stretch">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 pr-0-md pl-05-md text-xs-center flex-items-xs-stretch">
         <div class="card card-block">
             <h7 class="card-title">Nota Usuário</h5>
             <div class="value down mt-05 mb-05">9.1</div>
@@ -30,7 +30,7 @@
 </div>
 
 <div class="chart row ml-0 mr-0">
-    <div class="col-xs-12 col-md-6 col-lg-6 col-xl-6 pl-0-md pr-05-md text-xs-center flex-items-xs-stretch">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pl-0-md pr-05-md text-xs-center flex-items-xs-stretch">
         <div class="card card-block">
             <div class="charts">
                 <div class="row ml-0 mr-0 flex-items-xs-middle">
@@ -41,7 +41,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xs-12 col-md-6 col-lg-6 col-xl-6 pr-0-md pl-05-md text-xs-center flex-items-xs-stretch">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 pr-0-md pl-05-md text-xs-center flex-items-xs-stretch">
         <div class="card card-block">
             <div class="charts">
                 <div class="row ml-0 mr-0 flex-items-xs-middle">
