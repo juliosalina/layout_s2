c265115fb249b8e02a9cd29ec8a3dcefa4c4d5a6
Julio Henrique Salina <juliohsalina@gmail.com>
2017-01-05 11:43:39 -0200

    ajuste

---
+++ new/templates/cirurgias_pos.php
@@ -0,0 +1,148 @@
+<div class="cirurgia_pos row ml-0 mr-0">
+
+    <!-- card misto-->
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 pl-0-md pr-05-md flex-items-xs-stretch">
+        <div class="card card-secondary">
+            <div class="card-header">Paciente</div>
+            <div class="row card-block">
+
+                <div class="block-item">
+                    <i class="fa fa-eye fa-lg"></i>
+                </div>
+
+                <div class="col-xs-3 pl-0-md pr-05-md flex-items-xs-middle">
+                    <img src="assets/images/users/foto_perfil.jpg" alt="alt-img" class="rounded-circle" width="100" height="100">
+                </div>
+
+                <div class="col-xs-7 col-md-9 plr-05-md pr-0-lg">
+                    <ul class="list-group">
+                        <li class="list-group-item"><label>Nome:</label>Bruno Galati</li>
+                        <li class="list-group-item"><label>Apelido:</label>Bruno</li>
+                        <li class="list-group-item"><label>CPF:</label>123.123.123-12</li>
+                        <li class="list-group-item"><label>Dt. Nascimento:</label>04/11/1983 (33 anos)</li>
+                        <li class="list-group-item"><label>Tel. Fixo:</label></li>
+                        <li class="list-group-item"><label>Tel. Celular:</label>(11) 9815-64900</li>
+                        <li class="list-group-item"><label>Email:</label>brngalati@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- card azul-->
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 plr-05-md pr-0-lg flex-items-xs-stretch">
+        <div class="card card-inverse card-primary">
+            <div class="card-header">Status Cirurgia</div>
+            <div class="card-block">
+                <h4 class="card-title">Em análise</h4>
+                <p class="card-text">Some quick <b>example</b> text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- card misto-->
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 pr-0-md pl-05-md flex-items-xs-stretch">
+        <div class="card card-secondary">
+            <div class="card-header">Agendamentos</div>
+            <div class="row card-block">
+
+                <div class="block-item">
+                    <i class="fa fa-eye fa-lg"></i>
+                    <i class="fa fa-eye fa-lg"></i>
+                    <i class="fa fa-eye fa-lg"></i>
+                </div>
+
+                <div class="col-xs-12 col-md-12 plr-05-md pr-0-lg">
+                    <ul class="list-group">
+                        <li class="list-group-item"><label>Data:</label>27/12/2016 - 07:45</li>
+                        <li class="list-group-item"><label>Produto:</label>CONSULTA NUTRICIONISTA</li>
+                        <li class="list-group-item"><label>Profissional:</label>Mirelle Maira Santos</li>
+                        <li class="list-group-item"><label>Clínica:</label>ITAQUERA</li>
+                        <li class="list-group-item"><label>Valor</label>R$ 90,00</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<!-- blocos fundo cinza -->
+<div class="cirurgia_pos row ml-0 mr-0">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 pl-0-md pr-0-md flex-items-xs-stretch">
+        <div class="card card-block">
+            <h4 class="card-title">Dados da Cirurgia 1</h4>
+            <div class="row">
+
+                <div class="col-md-2 pl-0-md pr-05-md flex-items-xs-stretch">
+                    <div class="col-xs-12 gray-bkg flex-items-xs-middle p-05">
+                        <i class="fa fa-hospital-o fa-2x"></i>
+                        <ul class="list-group">
+                            <li class="list-group-item"><label>Label:</label>Cras justo odio</li>
+                            <li class="list-group-item"><label>Label:</label>Dapibus ac facilisi</li>
+                            <li class="list-group-item"><label>maior:</label>Morbi leo</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2 plr-05-md flex-items-xs-stretch">
+                    <div class="col-xs-12 gray-bkg flex-items-xs-middle p-05">
+                        <i class="fa fa-user-md fa-2x"></i>
+                        <ul class="list-group">
+                            <li class="list-group-item"><label>Label:</label>Cras justo odio</li>
+                            <li class="list-group-item"><label>Label:</label>Dapibus ac facilisis</li>
+                            <li class="list-group-item"><label>Label maior:</label>Morbi leo risus</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2 plr-05-md flex-items-xs-stretch">
+                     <div class="col-xs-12 gray-bkg flex-items-xs-middle p-05">
+                        <i class="fa fa-calendar fa-2x"></i>
+                        <ul class="list-group">
+                            <li class="list-group-item"><label>Label:</label>Cras justo odio</li>
+                            <li class="list-group-item"><label>Label:</label>Dapibus ac facilisis</li>
+                            <li class="list-group-item"><label>Label maior:</label>Morbi leo risus</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2 pl-05-md pr-1 flex-items-xs-stretch">
+                     <div class="col-xs-12 gray-bkg flex-items-xs-middle p-05">
+                        <i class="fa fa-calendar-check-o fa-2x"></i>
+                        <ul class="list-group">
+                            <li class="list-group-item"><label>Label:</label>Cras justo odio</li>
+                            <li class="list-group-item"><label>Label:</label>Dapibus ac facilisi</li>
+                            <li class="list-group-item"><label>maior:</label>Morbi leo</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- botoes -->
+
+                <div class="row col-md-4 pr-0-md pl-05-md brd-l">
+
+                  <div class="col-md-6 plr-05-md mb-05">
+                      <button type="button" class="btn btn-secondary btn-block light-text">Retorno Cirurgião</button>
+                  </div>
+                  <div class="col-md-6 pl-05-md pr-0-md mb-05">
+                      <button type="button" class="btn btn-secondary btn-block light-text">Retorno Cirurgião</button>
+                  </div>
+                  <div class="col-md-6 plr-05-md mb-05">
+                      <button type="button" class="btn btn-secondary btn-block light-text">Retorno Cirurgião</button>
+                  </div>
+                  <div class="col-md-6 pl-05-md pr-0-md mb-05">
+                      <button type="button" class="btn btn-secondary btn-block light-text">Retorno Cirurgião</button>
+                  </div>
+                  <div class="col-md-6 plr-05-md mb-05">
+                      <button type="button" class="btn btn-secondary btn-block light-text">Retorno Cirurgião</button>
+                  </div>
+                  <div class="col-md-6 pl-05-md pr-0-md mb-05">
+                      <button type="button" class="btn btn-secondary btn-block light-text">Retorno Cirurgião</button>
+                  </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
